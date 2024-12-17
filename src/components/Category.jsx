d022bcaed35170a060d5a73a342d00d7a4aa36f5
lab1pc4 <lab1pc4@tester.com>
2024-12-17 14:52:43 +0500

    save

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,10 +1,81 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Nav from './Nav'
-
+import AllData from './Data'
 function Category() {
+  let [data , setData] =useState(AllData)
+  let [inputVal , setInputVal] = useState()
+  if(inputVal == "Product A to Z"){
+    data.sort((a,b)=>{
+      let nameA = a.name
+      let nameB = b.name
+      if(nameA < nameB){
+        console.log(nameA);
+        console.log(nameB);
+        return -1
+      }
+    })
+  }
+  if(inputVal == "Product Z to A"){
+    data.sort((a,b)=>{
+      let nameA = a.name
+      let nameB = b.name
+      if(nameA > nameB){
+        console.log(nameA);
+        console.log(nameB);
+        return -1
+      }
+    })
+  }
+  if(inputVal == "Price High to Low"){
+    data.sort((a,b)=>{
+      let priceA = a.price
+      let priceB = b.price
+      return priceB - priceA
+     
+      
+    })
+  }
+  if(inputVal == "Price Low to High"){
+    data.sort((a,b)=>{
+      let priceA = a.price
+      let priceB = b.price
+      return priceA - priceB
+     
+      
+    })
+  }
+
   return (
    <>
-   <Nav/>
+     <div className="container">
+      <Nav/>
+        <div className="category">
+          <h2 style={{color : "white"}}>Category</h2>
+          <select name="" id="" onChange={(e)=>setInputVal(e.target.value)}>
+            <option value="Product A to Z">Product A to Z</option>
+            <option value="Product Z to A">Product Z to A</option>
+            <option value="Price High to Low">Price High to Low</option>
+            <option value="Price Low to High">Price Low to High</option>
+          </select>
+        </div>
+        <div className="product">
+          {
+            data.map((curr ,index)=>{
+              return(
+                <div className="card" key={index}>
+                  <img src={curr.image} alt="" />
+                  <div className="text">
+                    <h2>Name : {curr.name}</h2>
+                    <h2>Price : {curr.price}</h2>
+                    <p><mark>Description:</mark>{curr.description}</p>
+                    <button>{curr.button}</button>
+                  </div>
+                </div>
+              )
+            })
+          }
+        </div>
+     </div>
    </>
   )
 }
--- src/components/Data.js
+++ src/components/Data.js
@@ -1,5 +1,3 @@
-import asus from '../assets/images/asus.jfif'
-import dell from '../assets/images/dell.jfif'
 import hp from '../assets/images/hp.jfif'
 import mac from '../assets/images/mac.jfif'
 import razer from '../assets/images/razer.jfif'
@@ -8,60 +6,67 @@ import lenovo from '../assets/images/lenovo.jfif'
 import samsung from '../assets/images/samsung.jfif'
 
 const AllData = [
+   
+
     {
         id : 1,
-        name : "Dell",
-        price : 40000,
-        image : dell
+        name : "HP",
+        price : 50000,
+        image : hp,
+        button : "Buy Now",
+        description : "The unibody construction also has a black plastic keyboard that was first used on the MacBook Air, which itself was inspired by the sunken keyboard of the original polycarbonate MacBooks. The now standardized keyboard brings congruity to the MacBook line, with black keys on a metallic aluminum body.",
     },
+
+   
 
     {
         id : 2,
-        name : "HP",
-        price : 50000,
-        image : hp
+        name : "Sony",
+        price : 70000,
+        image : sony,
+        button : "Buy Now",
+        description : "The unibody construction also has a black plastic keyboard that was first used on the MacBook Air, which itself was inspired by the sunken keyboard of the original polycarbonate MacBooks. The now standardized keyboard brings congruity to the MacBook line, with black keys on a metallic aluminum body.",
+
     },
 
     {
         id : 3,
-        name : "ASUS",
-        price : 120000,
-        image : asus
+        name : "Mac",
+        price : 90000,
+        image : mac,
+        button : "Buy Now",
+        description : "The unibody construction also has a black plastic keyboard that was first used on the MacBook Air, which itself was inspired by the sunken keyboard of the original polycarbonate MacBooks. The now standardized keyboard brings congruity to the MacBook line, with black keys on a metallic aluminum body.",
+
     },
 
     {
         id : 4,
-        name : "Sony",
-        price : 70000,
-        image : sony
+        name : "Samsung",
+        price : 120000,
+        image : samsung,
+        description : "The unibody construction also has a black plastic keyboard that was first used on the MacBook Air, which itself was inspired by the sunken keyboard of the original polycarbonate MacBooks. The now standardized keyboard brings congruity to the MacBook line, with black keys on a metallic aluminum body.",
+        button : "Buy Now",
+
     },
 
     {
         id : 5,
-        name : "Mac",
-        price : 90000,
-        image : mac
+        name : "Lenovo",
+        price : 75000,
+        image : lenovo,
+        description : "The unibody construction also has a black plastic keyboard that was first used on the MacBook Air, which itself was inspired by the sunken keyboard of the original polycarbonate MacBooks. The now standardized keyboard brings congruity to the MacBook line, with black keys on a metallic aluminum body.",
+        button : "Buy Now",
+
     },
 
     {
         id : 6,
-        name : "Samsung",
-        price : 120000,
-        image : samsung
-    },
-
-    {
-        id : 7,
-        name : "Lenovo",
-        price : 75000,
-        image : lenovo
-    },
-
-    {
-        id : 8,
         name : "Razer",
         price : 180000,
-        image : razer
+        image : razer,
+        button : "Buy Now",
+        description : "The unibody construction also has a black plastic keyboard that was first used on the MacBook Air, which itself was inspired by the sunken keyboard of the original polycarbonate MacBooks. The now standardized keyboard brings congruity to the MacBook line, with black keys on a metallic aluminum body.",
+
     },
 ]
 
